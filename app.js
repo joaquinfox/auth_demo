524cc663233000d09064efa5ae40ede52cd4d743
Joaquin Fox <Joaquin.Fox@gmail.com>
2020-07-02 17:51:45 -0700

    models, db setup, dependencies imported, routes and views built out

--- app.js
+++ app.js
@@ -1,10 +1,44 @@
 const express = require('express');
+const app = express();
 const mongoose = require('mongoose');
 const session = require('express-session');
 const passport = require('passport');
-const LocalStrategy = require('passport-local')
+const LocalStrategy = require('passport-local');
+const User = require('./models/user');
 
+mongoose.connect('mongodb://localhost/auth_demo', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-express()
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app
   .set('view engine', 'ejs')
-  .use(express.urlencoded({ extended: true }));
+  .use(express.urlencoded({ extended: true }))
+  .use(
+    session({
+      secret: 'jdakjda',
+      saveUninitialized: true,
+      resave: true,
+    })
+  )
+  .use(passport.initialize())
+  .use(passport.session())
+  .get('/', (req, res) => {
+    res.render('register');
+  })
+  .get('/secret', (req, res) => {
+    res.render('secret');
+  })
+  .get('/register', (req, res) => {
+    res.render('register');
+  })
+  .get('/login', (req, res) => {
+    res.render('login');
+  })
+  .listen(process.env.PORT || 3000, () => {
+    console.log('listening');
+  });
